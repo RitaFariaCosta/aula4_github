4451718b8bab0fe207412751064ac24f1dd8d015
RitaFariaCosta <alicecooperbloosom@gmail.com>
2024-03-05 23:35:25 -0300

    adicionei meu nome como nomedocolega
    adicionei o nome arianne como nomedocolega no programa da rita

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@ using namespace std;
 int main()
 {
     string nome = "Rita";
+    string nomedocolega = "Arianne";
     int idade = 17;
     cout << idade << endl;
     cout<<nome << endl;
+    cout << nome_do_colega << endl;
 
     return 0;
 }
